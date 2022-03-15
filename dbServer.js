d7bc10a93da6cc742c7e9f05b156a3c9a282ce89
nishnak <nishna202@gmail.com>
2022-03-15 23:04:53 +0530

    nishnak commit

--- dbServer.js
+++ dbServer.js
@@ -4,8 +4,7 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_DATABASE = process.env.DB_DATABASE
 const DB_PORT = process.env.DB_PORT
-
-hello
+hello hi bye
 const express = require("express")
 const app = express()
 const mysql = require("mysql")
