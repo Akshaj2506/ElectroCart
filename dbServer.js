ad532fe40fcba5228c9d01def8c72180686e1a86
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2022-03-15 23:19:55 +0530

    commit 1

--- dbServer.js
+++ dbServer.js
@@ -4,7 +4,7 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_DATABASE = process.env.DB_DATABASE
 const DB_PORT = process.env.DB_PORT
-Hello Nishna
+
 const express = require("express")
 const app = express()
 const mysql = require("mysql")
@@ -20,7 +20,7 @@ db.getConnection( (err, connection)=> {
    if (err) throw (err)
    console.log ("DB connected successful: " + connection.threadId)
 })
-hi bye
+
 const port = process.env.PORT
 
 app.listen(port,
